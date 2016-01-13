20a3cabe525a4551e7edf694f8bc5ed7bdb8813e
Stanislav Matsel <stanislav.matsel@ee.abb.com>
2016-01-13 19:37:28 +0200

    Model, View, Controller, Routing to basic MVC application

---
+++ PartyInvitesVS2015/PartyInvitesVS2015.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PartyInvitesVS2015.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PartyInvitesVS2015/PartyInvitesVS2015/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvitesVS2015.Models;
+
+namespace PartyInvitesVS2015.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // GET: Home
+        //public string Index()
+        //{
+        //    return "Hello, World!";
+        //}
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good mooning" : "Good day";
+            return View();
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            return View("Thanks", guest);
+        }
+    }
+}
