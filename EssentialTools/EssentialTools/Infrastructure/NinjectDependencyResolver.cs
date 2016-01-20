17757e825d4d145cefdecad26d2252ff3aea1c71
Stanislav Matsel <stanislav.matsel@ee.abb.com>
2016-01-20 19:40:53 +0200

    Add comment

--- EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -31,7 +31,7 @@ namespace EssentialTools.Infrastructure
 
         private void AddBindings()
         {
-            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope();
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope(); //create only one object from DI for http request
             //kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
             kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
             kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedExactlyInto<LinqValueCalculator>();
