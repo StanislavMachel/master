a1dbfd01fa790e46f97a5b5ebaddd563a398242a
Stanislav Matsel <stanislav.matsel@ee.abb.com>
2016-01-20 19:37:36 +0200

    Object live scope

--- EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -31,7 +31,7 @@ namespace EssentialTools.Infrastructure
 
         private void AddBindings()
         {
-            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope();
             //kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
             kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
             kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedExactlyInto<LinqValueCalculator>();
