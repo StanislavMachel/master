8fd9b1575b32631f26f161bd49fee2397aa07695
Stanislav Matsel <stanislav.matsel@ee.abb.com>
2016-01-20 19:44:59 +0200

    Add class for Unit Test

---
+++ EssentialTools/EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTools.Models
+{
+    class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
